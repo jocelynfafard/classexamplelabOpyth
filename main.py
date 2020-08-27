ec6452b6a0b21c1102365f2ef29ee06ca78ec7c4
Jocelyn Fafard <jqf5530@psu.edu>
2020-08-27 21:41:45 +0000

    E

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 name = input("Enter your name: ")
 print("Hello, " + name + "!")
-age = input("enter your age: ")
+age = input("Enter your age: ")
 age = int(age);
 print("You will be " + str(age+1) + " next year.")
